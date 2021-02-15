bd17bc2dc8b6155e1d3dfe5d9cc655caa74f1373
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-02-14 22:01:39 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- 
 select 
 	e.emp_no as "Employee number",
 	e.first_name as "First Name",
@@ -5,7 +6,7 @@ select
 	t.title as "Title",
 	t.from_date as "Title From Date",
 	t.to_date as "Title to Date"
---into retirement_titles
+into retirement_titles
 from employees e 
 inner join titles t on e.emp_no = t.emp_no 
 where e.birth_date between '1952-01-01' AND '1955-12-31'
@@ -13,7 +14,7 @@ order by e.emp_no
 ;
 
 
--- Use Dictinct with Orderby to remove duplicate rows
+-- Gets the Current Titles of Retiring employees
 SELECT 
 	DISTINCT ON (rt.emp_no) rt.emp_no,
 	rt.first_name,
@@ -23,13 +24,15 @@ into unique_titles
 FROM retirement_titles rt 
 ORDER BY rt.emp_no , rt.to_date DESC;
 
-
-select count (title),title
+-- Gets count of retiring employees by title
+select count (title) as title_count,title
 into retiring_titles
+into rit_count
 from unique_titles
 group by title
 order by count(title) desc ;
 
+--Gets employees who are eligible to be mentors
 select 
 	distinct on (e.emp_no) e.emp_no,
 	e.first_name,
@@ -43,4 +46,21 @@ from employees e
 inner join dept_emp de on de.emp_no = e.emp_no 
 inner join titles t on t.emp_no =e.emp_no 
 where e.birth_date between '1965-01-01' AND '1965-12-31'
-order by e.emp_no 
+order by e.emp_no ;
+
+--Gets count of mentors by title
+select me. title, count(me.title) as mentor_count
+into mentor_count
+from mentorship_eligibilty me
+group by me.title 
+order by count(me.title) desc ;
+
+-- Compares positions that need to be filled by eligible mentors
+select rc.title as "Title", 
+rc.title_count as "Retiree Count", 
+mc.mentor_count as "Mentor Count", 
+(rc.title_count/mc.mentor_count) as "Role per Mentor"
+from rit_count rc 
+full outer join mentor_count mc on mc.title = rc.title
+
+
